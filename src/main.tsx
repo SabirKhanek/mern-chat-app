d7f701c95627d76d37d53c1a899b6905ecca5dd3
Lubna <lubnakhizar24@gmail.com>
2023-09-20 12:10:14 +0500

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css'
-
+console.log('hi')
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
